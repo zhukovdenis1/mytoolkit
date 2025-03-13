835dfb2addd763f09165d55cb38f746667aebfc1
Zhukov Denis <zd1@list.ru>
2025-03-13 09:58:32 +0400

    note text to json migration

---
+++ app/Modules/Note/Database/Migrations/2025_03_13_055623_change_text_to_json_in_notes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Изменяем тип поля text с TEXT на JSON
+        Schema::table('notes', function (Blueprint $table) {
+            $table->json('text')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Возвращаем тип поля text обратно в TEXT
+        Schema::table('notes', function (Blueprint $table) {
+            $table->text('text')->change();
+        });
+    }
+};
